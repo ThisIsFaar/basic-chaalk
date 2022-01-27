7568048148971e67236cd846e03809dbf5f29fbe
thisisfaar <thisisfarhan99@gmail.com>
2022-01-27 16:46:16 +0530

    header done

---
+++ client/src/components/navbar/navbar.jsx
@@ -0,0 +1,48 @@
+import "./navbar.css";
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-nav-clr">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          <img
+            src="http://www.thechaalk.com/assets/images/logo-2.png"
+            alt="brand image"
+          ></img>
+        </a>
+        <div
+          className="collapse navbar-collapse d-flex justify-content-end"
+          id="navbarSupportedContent"
+        >
+          <ul className="navbar-nav d-flex align-items-center">
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                help
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Mckinze
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                <img
+                  className="rounded-circle"
+                  src="https://picsum.photos/id/1027/40/40"
+                  alt="brand image"
+                ></img>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
