331d2124f13b9562d930a4c186c405f8554dce85
thisisfaar <thisisfarhan99@gmail.com>
2022-01-27 16:47:26 +0530

    footer done

--- client/src/components/footer/footer.jsx
+++ client/src/components/footer/footer.jsx
@@ -1,7 +1,44 @@
 import "./footer.css";
 
 function Footer() {
-  return chec;
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-nav-clr">
+      <div className="container-fluid d-flex justify-content-center">
+        <ul className="navbar-nav d-flex align-items-center">
+          <li className="nav-item rounded-circle icons-btm mx-2">
+            <a className="nav-link text-white" href="">
+              <i class="fas fa-microphone"></i>
+            </a>
+          </li>
+          <li className="nav-item rounded-circle icons-btm mx-2 rounded-circle">
+            <a className="nav-link text-white" href="">
+              <i class="fas fa-video"></i>
+            </a>
+          </li>
+          <li className="nav-item rounded-circle icons-btm mx-2">
+            <a className="nav-link text-white" href="">
+              <i class="far fa-closed-captioning"></i>
+            </a>
+          </li>
+          <li className="nav-item rounded-circle icons-btm mx-2">
+            <a className="nav-link text-white" href="">
+              <i class="fas fa-external-link-alt"></i>
+            </a>
+          </li>
+          <li className="nav-item rounded-circle icons-btm mx-2">
+            <a className="nav-link text-white" href="">
+              <i class="fas fa-ellipsis-v"></i>
+            </a>
+          </li>
+          <li className="nav-item rounded-circle icons-btm bg-danger mx-2">
+            <a className="nav-link text-white" href="">
+              <i class="fas fa-phone-slash"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
 export default Footer;
